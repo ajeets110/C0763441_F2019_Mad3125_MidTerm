71faf834536b3f6da2ff3a0b04f39ec2d6033aca
macstudent <ajeets110@gmail.com>
2019-12-02 17:17:13 -0500

    added view on display acticity

--- app/src/main/java/com/example/c0763441_f2019_mad3125_midterm/DisplayActivity.java
+++ app/src/main/java/com/example/c0763441_f2019_mad3125_midterm/DisplayActivity.java
@@ -3,8 +3,17 @@ package com.example.c0763441_f2019_mad3125_midterm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DisplayActivity extends AppCompatActivity {
+
+    CRACustomer customer;
+    TextView txt_sin;
+    TextView txt_full_Name;
+    TextView txt_gender;
+    TextView txt_gross_income, txt_taxDate, txt_federal_tax, txt_provincialTax, lbl_cpp,
+            lbl_empInsurance, lbl_RrspCcontributed, lbl_Cf_RRSP,
+            lbl_taxableIncome, lbl_taxPaid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
