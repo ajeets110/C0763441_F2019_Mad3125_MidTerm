5e0298a7059e735ab95affeb1cccf5759b9ec1b2
macstudent <ajeets110@gmail.com>
2019-12-02 14:23:02 -0500

    added variables in CRA

--- app/src/main/java/com/example/c0763441_f2019_mad3125_midterm/CRACustomer.java
+++ app/src/main/java/com/example/c0763441_f2019_mad3125_midterm/CRACustomer.java
@@ -1,4 +1,13 @@
 package com.example.c0763441_f2019_mad3125_midterm;
 
 public class CRACustomer {
+
+    int sin_number;
+    String first_name;
+    String  last_name;
+    String full_name=last_name.toUpperCase()+","+first_name;
+    double gross_income;
+    double rrsp_contribution;
+
+
 }
