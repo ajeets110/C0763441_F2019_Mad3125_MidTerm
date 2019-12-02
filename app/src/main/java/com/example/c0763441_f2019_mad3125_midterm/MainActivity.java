ad98dac243bfd216a134d9979d711fc5cef4e1e4
macstudent <ajeets110@gmail.com>
2019-12-02 14:53:41 -0500

    added intent on submit button

--- app/src/main/java/com/example/c0763441_f2019_mad3125_midterm/MainActivity.java
+++ app/src/main/java/com/example/c0763441_f2019_mad3125_midterm/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.c0763441_f2019_mad3125_midterm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -68,6 +69,15 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //submit button
+        btn_submit=findViewById(R.id.btnSubmit);
+        btn_submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this, DisplayActicity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 
