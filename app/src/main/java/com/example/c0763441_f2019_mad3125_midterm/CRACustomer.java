350c2aba15b26af9834849d18786395b4f5b8287
macstudent <ajeets110@gmail.com>
2019-12-02 18:38:09 -0500

    working on displaying data

--- app/src/main/java/com/example/c0763441_f2019_mad3125_midterm/CRACustomer.java
+++ app/src/main/java/com/example/c0763441_f2019_mad3125_midterm/CRACustomer.java
@@ -15,12 +15,8 @@ public class CRACustomer implements Parcelable {
     String gender;
     String full_name;
     Date dob,filingDate;
-
     double gross_income;
     double rrsp_contribution;
-    double EI;
-    double total_taxable_amount=(gross_income - (cppAmount()+rrspAmount()+eiAmount()));
-    double total_tax_paid=provincialTax()+federalTax();
 
     public Date getDob() {
         return dob;
@@ -99,46 +95,6 @@ public class CRACustomer implements Parcelable {
         this.gender = gender;
     }
 
-    // calculating CPP amount
-    public double cppAmount(){
-        double cpp_slab=57400.00;
-        double cpp_rate=5.10;
-        double actual_cpp=0.0;
-        if(gross_income>=cpp_slab)
-        {
-            actual_cpp=(cpp_slab*cpp_rate)/100;
-        }
-        else {
-            actual_cpp=(gross_income*cpp_rate)/100;
-        }
-        return actual_cpp;
-    }
-
-    // Calculating RRSP Amount
-    public double rrspAmount(){
-        double rrsp_perc=18.00;
-        double actual_rrsp=(gross_income*rrsp_perc)/100;
-        if(actual_rrsp>rrsp_contribution) {
-            System.out.println("RRSP amount exceeded ,You may have to face penalty");
-        }
-        return actual_rrsp;
-    }
-
-    // C a l c u l a t i n g   E I   a m o u n t
-    public double eiAmount(){
-
-        double ei_slab=53100.00;
-        double ei_rate=1.62;
-        double actual_ei=0.0;
-        if(gross_income>=ei_slab)
-        {
-            actual_ei=(ei_slab * ei_rate)/100;
-        }
-        else {
-            actual_ei=(gross_income * ei_rate)/100;
-        }
-        return actual_ei;
-    }
     // http://zetcode.com/java/numberformat/
 
     public String amountFomatter()
@@ -164,8 +120,7 @@ public class CRACustomer implements Parcelable {
         parcel.writeString(full_name);
         parcel.writeDouble(gross_income);
         parcel.writeDouble(rrsp_contribution);
-        parcel.writeDouble(EI);
-        parcel.writeDouble(total_taxable_amount);
+
 
     }
 
@@ -175,7 +130,6 @@ public class CRACustomer implements Parcelable {
         this.last_name = last_name;
         this.gross_income = gross_income;
         this.rrsp_contribution = rrsp_contribution;
-        this.EI = eiAmount();
         this.gender = gender;
     }
 
@@ -190,90 +144,6 @@ public class CRACustomer implements Parcelable {
         rrsp_contribution = parcel.readDouble();
 
     }
-    // Calculating provincial tax
-    public double provincialTax(){
-        double pro_tax=0.0;
-
-        double first_slab_perc=5.05;
-        double first_slab=33324;
-
-        double second_slab_perc=9.15;
-        double second_slab=43907;
-
-        double third_slab_perc=11.16;
-        double third_slab=62187;
-
-        double fourth_slab_perc=12.16;
-        double fourth_slab=70000;
-
-        double final_slab=0.01;
-        double final_slab_perc=13.16;
-        total_taxable_amount=total_taxable_amount-10582.00;
-        if(total_taxable_amount<=first_slab) {
-            pro_tax = (first_slab * first_slab_perc) / 100;
-            total_taxable_amount = total_taxable_amount - first_slab;
-        }
-
-        if(total_taxable_amount<=second_slab) {
-            pro_tax = (second_slab * second_slab_perc) / 100;
-            total_taxable_amount = total_taxable_amount - second_slab;
-        }
-        if(total_taxable_amount<=third_slab) {
-            pro_tax = (third_slab * third_slab_perc) / 100;
-            total_taxable_amount = total_taxable_amount - third_slab;
-        }
-        if(total_taxable_amount<=fourth_slab) {
-            pro_tax = (fourth_slab * fourth_slab_perc) / 100;
-            total_taxable_amount = total_taxable_amount - fourth_slab;
-        }
-        if(total_taxable_amount<=final_slab) {
-            pro_tax=(final_slab * final_slab_perc)/100;
-        }
-        return pro_tax;
-    }
-
-    // Calculating Federal tax
-    public double federalTax(){
-        double fed_tax=0.0;
-
-        double first_slab_perc=15.00;
-        double first_slab=35561;
-
-        double second_slab_perc=20.50;
-        double second_slab=47628.99;
-
-        double third_slab_perc=26.00;
-        double third_slab=52407.99;
-
-        double fourth_slab_perc=29.00;
-        double fourth_slab=60703.99;
-
-        double final_slab=0.01;
-        double final_slab_perc=33.00;
-        total_taxable_amount=total_taxable_amount-12069.00;
-        if(total_taxable_amount>=first_slab) {
-            fed_tax = (first_slab * first_slab_perc) / 100;
-            total_taxable_amount = total_taxable_amount - first_slab;
-        }
-
-        if(total_taxable_amount>=second_slab) {
-            fed_tax = (second_slab * second_slab_perc) / 100;
-            total_taxable_amount = total_taxable_amount - second_slab;
-        }
-        if(total_taxable_amount>=third_slab) {
-            fed_tax = (third_slab * third_slab_perc) / 100;
-            total_taxable_amount = total_taxable_amount - third_slab;
-        }
-        if(total_taxable_amount>=fourth_slab) {
-            fed_tax = (fourth_slab * fourth_slab_perc) / 100;
-            total_taxable_amount = total_taxable_amount - fourth_slab;
-        }
-        if(total_taxable_amount>=final_slab) {
-            fed_tax=(final_slab * final_slab_perc)/100;
-        }
-        return fed_tax;
-    }
-
 
 
 }
--- app/src/main/java/com/example/c0763441_f2019_mad3125_midterm/DisplayActivity.java
+++ app/src/main/java/com/example/c0763441_f2019_mad3125_midterm/DisplayActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class DisplayActivity extends AppCompatActivity {
 
@@ -14,13 +15,13 @@ public class DisplayActivity extends AppCompatActivity {
     TextView txt_gender;
     TextView txt_gross_income, txt_taxDate, txt_federal_tax, txt_provincialTax, lbl_cpp, lbl_empInsurance, lbl_RrspContributed, lbl_Cf_RRSP, lbl_taxableIncome, lbl_taxPaid;
     double cpp = 0;
-    double ei = 0;
     double rrsp = 0;
     double rrspCf = 0;
-    double taxableIncome;
+    double totalTaxableAmount;
     double federalTax;
     double provincialTax;
     double totalTaxPaid;
+    double emp_Insurance;
 
 
     @Override
@@ -30,7 +31,7 @@ public class DisplayActivity extends AppCompatActivity {
 
         txt_sin = findViewById(R.id.txt_Sin);
         txt_full_Name = findViewById(R.id.txt_fullName);
-        txt_gender =   findViewById(R.id.txt_gender);
+        txt_gender = findViewById(R.id.txt_gender);
         txt_gross_income = findViewById(R.id.txt_grossIncome);
         lbl_RrspContributed = findViewById(R.id.txt_rrspContribution);
         lbl_cpp = findViewById(R.id.txt_CppYear);
@@ -43,18 +44,19 @@ public class DisplayActivity extends AppCompatActivity {
 
 
         Intent mIntent = getIntent();
-        CRACustomer customer = mIntent.getParcelableExtra("CRACustomer");
+        CRACustomer customer = mIntent.getParcelableExtra("User_Data");
 
         txt_sin.setText(" SIN: \t" + customer.getSin_number());
         txt_full_Name.setText(" FULL NAME: \t" + customer.getFull_name());
         txt_gender.setText(" GENDER: \t" + customer.getGender());
         txt_gross_income.setText(" GROSS INCOME: \t" + customer.getGross_income());
         lbl_RrspContributed.setText("RRSP Contributed: \t" + customer.getRrsp_contribution());
-
+        rrsp = customer.getRrsp_contribution();
 
         // calculate  cpp
+
         double grossIncome = customer.getGross_income();
-        if(grossIncome > 57400.00){
+        if (grossIncome > 57400.00) {
             cpp = (57400.00 * 0.051); //5.10%
         } else {
             cpp = (grossIncome * 0.051);
@@ -62,18 +64,19 @@ public class DisplayActivity extends AppCompatActivity {
         lbl_cpp.setText("CPP COntribution in Year:\t" + cpp);
 
         // calculate employement insurance
-
-        if(grossIncome > 53100){
-            ei = (53100 * 0.0162); //1.62%
-        }else{
-            ei = (grossIncome * (1.62/100));
-        }
-        lbl_empInsurance.setText("Employeement Insurance: \t" + ei);
+            if (grossIncome > 53100) {
+                emp_Insurance = (53100 * 0.0162); //1.62%
+            } else {
+                emp_Insurance = (grossIncome * (1.62 / 100));
+            }
+        lbl_empInsurance.setText("Employeement Insurance: \t" + emp_Insurance);
 
         // calculate RRSP
+
         rrsp = customer.getRrsp_contribution();
         double maxRRSP = (grossIncome * 0.18); //18%
         if(rrsp > maxRRSP ){
+            Toast.makeText(this,"you mave have to face a penalty, amount exceeding",Toast.LENGTH_SHORT).show();
             rrspCf = rrsp - maxRRSP;
             rrsp = maxRRSP;
         }else{
@@ -82,88 +85,85 @@ public class DisplayActivity extends AppCompatActivity {
         lbl_Cf_RRSP.setText("RRSP Carry forward: \t"+ rrspCf);
 
         //taxable income
-        taxableIncome = grossIncome - (cpp + ei + rrsp);
-        lbl_taxableIncome.setText("Taxable income:\t" + (double) taxableIncome);
+            totalTaxableAmount = grossIncome - (cpp + emp_Insurance + rrsp);
+            lbl_taxableIncome.setText("Taxable income:\t" + (double) totalTaxableAmount);
 
-        //federal tax
+
+            /federal tax
         double calFederal = calcFedralTax();
-        txt_federal_tax.setText("Federal Tax: \t" + calFederal);
-
+        txtfederal_Tax.setText("Federal Tax: \t" + calFederal);
         // Provincial Tax
         double calProvincial = calcProvincialTax();
-
-        txt_provincialTax.setText("Provincial Tax:\t" + calProvincial);
+        txtprovincial_Tax.setText("Provincial Tax:\t" + calProvincial);
         // total tax paid
-        double taxpaid = calcTaxPaid();
-        lbl_taxPaid.setText("Total tax Paid:\t" + taxpaid);
-
+        double taxpaid = calTaxPaid();
+        lblTaxPaid.setText("Total tax Paid:\t" + taxpaid);
 
     }
 
-
-    public double calcFedralTax(){
+    public double calcFedralTax () {
         //calculate federal tax
-        double temp = taxableIncome ;
-        if(temp <= 12069.00){
+        double temp = totalTaxableAmount;
+        if (temp <= 12069.00) {
             federalTax = 0;//0%
-            temp = taxableIncome - 12069.00;
+            temp = totalTaxableAmount - 12069.00;
         }
-        if(temp >= 12069.01){
+        if (temp >= 12069.01) {
             federalTax = (temp * 0.15);//15%
             temp = temp - 35561;
         }
-        if(temp >= 47630.01){
+        if (temp >= 47630.01) {
             federalTax = (temp * 0.205); //20.50%
             temp = temp - 47628.99;
         }
-        if(temp >= 95259.01){
+        if (temp >= 95259.01) {
             federalTax = (temp * 0.26); //26%
             temp = temp - 52407.99;
         }
-        if (temp >= 147667.01){
+        if (temp >= 147667.01) {
             federalTax = (temp * 0.29);//29%
             temp = temp - 62703.99;
         }
-        if(temp >= 210371.01){
+        if (temp >= 210371.01) {
             federalTax = (temp * 0.33);//33%
             //temp = temp - federalTax;
         }
         return federalTax;
     }
 
-    public  double calcProvincialTax(){
+    public double calcProvincialTax () {
         //calculate provincial tax
-        double temp = taxableIncome ;
+        double temp = totalTaxableAmount;
 
-        if(temp <= 10582.00){
+        if (temp <= 10582.00) {
             provincialTax = 0;
-            temp = taxableIncome - 10582.00;
+            temp = totalTaxableAmount - 10582.00;
         }
-        if(temp >= 10582.01){
+        if (temp >= 10582.01) {
             provincialTax = (temp * 0.0505); //5.05%
             temp = temp - 33323.99;
         }
-        if(temp >= 43906.01){
+        if (temp >= 43906.01) {
             provincialTax = (temp * 0.0915); //9.15%
             temp = temp - 43906.99;
         }
-        if(temp >= 87813.01){
+        if (temp >= 87813.01) {
             provincialTax = (temp * 0.1116); //11.16%
             temp = temp - 62187.99;
         }
-        if (temp >= 150000.01){
+        if (temp >= 150000.01) {
             provincialTax = (temp * 0.1216);//12.16%
             temp = temp - 69999.99;
         }
-        if(temp >= 220000.01){
+        if (temp >= 220000.01) {
             provincialTax = (temp * 0.1316);//13.16%
 
         }
         return provincialTax;
     }
 
-    public  double calcTaxPaid(){
+    public double calcTaxPaid () {
         return totalTaxPaid = provincialTax + federalTax;
     }
-}
 
+}
